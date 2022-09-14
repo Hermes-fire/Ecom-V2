ba5c11b95e6b82c5c9e2a79e4e3b081810f4bc15
Hermes-fire <amine.sadali@gmail.com>
2022-09-14 10:15:10 +0100

    updated logout

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post('/register', validate(authValidation.register), authController.register);
 router.post('/login', validate(authValidation.login), authController.login);
 router.post('/logout', authController.logout);
-router.get('/refresh-tokens', authController.refreshTokens);
+router.post('/refresh-tokens', authController.refreshTokens);
 router.post('/forgot-password', validate(authValidation.forgotPassword), authController.forgotPassword);
 router.post('/reset-password', validate(authValidation.resetPassword), authController.resetPassword);
 router.post('/send-verification-email', auth(), authController.sendVerificationEmail);
